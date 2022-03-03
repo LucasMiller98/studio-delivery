3c35634eb5905a3efd1119b5f11c8e4d9599dba7
Cruz <lucas.miller.s.cruz@accenture.com>
2022-03-03 01:47:30 -0300

    Adding README

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,4 @@
+
+export function Home() {
+    return <h1>Welcome to home page</h1>
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Home } from '../pages/Home/Home';
 import { Loading } from '../pages/Loading'
 import { Login } from '../pages/Login';
 import { Register } from '../pages/register'
@@ -12,6 +13,7 @@ export function AllRoutes() {
         <Route path='/login' element={<Login />} />
         <Route path='/cadastre-se' element={<Register />} />
         <Route path='/cadastre-se/account' element={<RegisterSettingAccount />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </Router>
   )
